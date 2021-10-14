1dfabc8bf86226b1b7c5b5641f3526fd940ccdb1
Taisuke Fukuno <fukuno@jig.jp>
2021-10-15 06:23:36 +0900

    add opts

--- input-kana.js
+++ input-kana.js
@@ -2,8 +2,8 @@ import { KanaValidator } from "./KanaValidator.js";
 import { InputWithValidator } from "https://code4fukui.github.io/input-number/InputWithValidator.js";
 
 class InputKana extends InputWithValidator {
-  constructor() {
-    super(new KanaValidator());
+  constructor(opts) {
+    super(new KanaValidator(), opts);
   }
 }
 
